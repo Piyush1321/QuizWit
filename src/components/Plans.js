c14f7eb6c3fff74aaba8935a8ec0a2a5637740a2
Ishwar Baisla <ishwar2303@gmail.com>
2022-02-07 18:08:34 +0530

    Plan Card and footer
    Plan card updated and footer updated

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,50 +1,182 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import $ from 'jquery';
 import PlanCard from './util/PlanCard';
 
 function Plans() {
+    const [plans, setPlans] = useState([]);
     useEffect(() => {
-        $("#view-plans-btn").click(() => {
+        $(".view-plans-btn").click(() => {
             $('html, body').animate({
                 scrollTop: $(".plans").offset().top - 50
             }, 1);
         });
+
+        const tempPlans = [
+            {
+                name: 'Free',
+                amount: 0,
+                durationInMonths: 1,
+                contains: [
+                    {
+                        name: 'Create 2 exams',
+                        type: 1
+                    },
+                    {
+                        name: 'Add 2 section per exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Maximum 10 question per section',
+                        type: 1
+                    },
+                    {
+                        name: 'Timer Exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Section navigation control',
+                        type: 0
+                    },
+                    {
+                        name: 'Question navigation control',
+                        type: 0
+                    },
+                    {
+                        name: 'Shuffle questions',
+                        type: 0
+                    },
+                    {
+                        name: 'Syntax highlighting',
+                        type: 0
+                    },
+                    {
+                        name: 'Private exam add student group',
+                        type: 0
+                    },
+                    {
+                        name: 'Create user',
+                        type: 0
+                    }
+                ]
+            },
+            {
+                name: 'Silver',
+                amount: 500,
+                durationInMonths: 1,
+                contains: [
+                    {
+                        name: 'Create 3 exams',
+                        type: 1
+                    },
+                    {
+                        name: 'Add 3 section per exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Maximum 20 question per section',
+                        type: 1
+                    },
+                    {
+                        name: 'Timer Exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Section navigation control',
+                        type: 1
+                    },
+                    {
+                        name: 'Question navigation control',
+                        type: 0
+                    },
+                    {
+                        name: 'Shuffle questions',
+                        type: 0
+                    },
+                    {
+                        name: 'Syntax highlighting',
+                        type: 1
+                    },
+                    {
+                        name: 'Private exam add student group',
+                        type: 0
+                    },
+                    {
+                        name: 'Create 1 user',
+                        type: 1
+                    }
+                ]
+            },
+            {
+                name: 'Gold',
+                amount: 1000,
+                durationInMonths: 3,
+                contains: [
+                    {
+                        name: 'Create 5 exams',
+                        type: 1
+                    },
+                    {
+                        name: 'Add 5 section per exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Maximum 30 question per section',
+                        type: 1
+                    },
+                    {
+                        name: 'Timer Exam',
+                        type: 1
+                    },
+                    {
+                        name: 'Section navigation control',
+                        type: 1
+                    },
+                    {
+                        name: 'Question navigation control',
+                        type: 1
+                    },
+                    {
+                        name: 'Shuffle questions',
+                        type: 1
+                    },
+                    {
+                        name: 'Syntax highlighting',
+                        type: 1
+                    },
+                    {
+                        name: 'Private exam add student group',
+                        type: 1
+                    },
+                    {
+                        name: 'Create 1 user',
+                        type: 1
+                    }
+                ]
+            }
+        ];
+        setPlans(tempPlans);
+
     }, []);
   return (
+      <>
       <div className='plans'>
         <h1>Administrator Plans</h1>
         <div className='plan-row'>
-            <PlanCard 
-                name='Free'
-                amount='0'
-                contains={['Create 2 exams', 'Create 2 section per exam', 'Maximum 20 Questions in a section', 'Timer on Exam']}
-            />
-            <PlanCard
-                name='Silver'
-                amount='500'
-                contains={['Create 5 exams', 'Create 1 user', 'Create 3 section per exam', 'Maximum 50 Questions in a section', 'Timer on Exam/Section']}
-            />
-            <PlanCard
-                name='Gold'
-                amount='1000'
-                contains={['Create 10 exams', 'Create 3 user', 'Create 5 section per exam', 'Maximum 100 Questions in a section', 'Timer on Exam/Section/Question', 'Navigation settings', 'Private Add student group']}
-            />
-        </div>
-        <h1 className='mt-10'>Student Plans</h1>
-        <div className='plan-row'>
-            <PlanCard 
-                name='Free'
-                amount='100'
-                contains={['Attempt 2 exams']}
-            />
-            <PlanCard
-                name='Premium'
-                amount='300'
-                contains={['Create 5 exams', 'Create 1 user', 'Create 3 section per exam', 'Maximum 50 Questions in a section', 'Timer on Exam/Section']}
-            />
+            {
+                plans.map((d, key) => {
+                    return <PlanCard 
+                        key={key}
+                        name={d.name}
+                        amount={d.amount}
+                        durationInMonths={d.durationInMonths}
+                        contains={d.contains}
+                    />
+                })
+            }
         </div>
       </div>
+      </>
   );
 }
 
--- src/components/util/PlanCard.js
+++ src/components/util/PlanCard.js
@@ -10,15 +10,24 @@ function PlanCard(props) {
   }, []);
   
   return (
-    <div className='plan-card'>
+    <a className='plan-card' href=''>
+      <div className='plan-name'>{props.name}</div>
       <div>
-          <div className='plan-name'>{props.name}</div>
+          <i className='fas fa-rupee-sign card-rupee-sign'></i>
+          <div className='bubble-tl'></div>
+          <div className='bubble-tr'></div>
           <div className='plan-charge'>{props.amount} <i className='fas fa-rupee-sign ml-10'></i></div>
+          <div className='plan-duration'> For {props.durationInMonths} {props.durationInMonths > 1 ? 'months': 'month' }</div>
           <div className='plan-includes'>{contains.map((v, key) => {
-            return <li key={key}><i className='fas fa-check mr-10 tick'></i> {v}</li>
+            return <li key={key} className='flex-row plan-items'>
+              <div>
+                <i className={'fas ' + (v.type ? 'fa-check ': 'fa-times ') + 'mr-10 ' + (v.type ? 'available': 'not-available')}></i> 
+              </div>
+              <p>{v.name}</p>
+              </li>
           })}</div>
       </div>
-    </div>
+    </a>
   );
 }
 
