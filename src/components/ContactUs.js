9aed72ee1632efaab9ee9d6f4cb2d7bbc0fd5302
Ishwar Baisla <ishwar2303@gmail.com>
2022-02-07 19:32:40 +0530

    Textarea css updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,34 +5,34 @@ import './css/ContactUs.css';
 
 function ContactUs() {
   return (
-      <div className='contact-us m-20 flex-row'>
+      <div className='contact-us flex-row'>
         <div>
           <form action="">
             <div className="header primary">Contact Us</div>
             <div class="input-block">
               <div class="input-container-floating-bb">
-                <input type="text" name='fullName' />
+                <input type="text" name='fullName' required/>
                 <label>Full Name</label>
                 <div class="respose"></div>
               </div>
             </div>
             <div className="input-block">
               <div class="input-container-floating-bb">
-                  <input type="email" name='email' />
+                  <input type="email" name='email' required/>
                   <label>Email</label>
                   <div class="respose"></div>
               </div>
             </div>
             <div className="input-block">
               <div class="input-container-floating-bb">
-                <input type="number" name='contact' />
+                <input type="number" name='contact' required/>
                 <label>Contact</label>
                 <div class="respose"></div>
               </div>
             </div>
             <div className="input-block">
               <div class="input-container-floating-bb">
-                <textarea name="query" id="" cols="30" rows="10"></textarea>
+                <textarea name="query" id="" cols="30" rows="10" required></textarea>
                 <label>Contact</label>
                 <div class="respose"></div>
               </div>
